86a6efcf05704f85d5faf6182ce5456e8b2e043c
Lilian Matsumoto <liliansmatsumoto@gmail.com>
2023-02-23 02:36:10 -0300

    criação def buscar_lojas no LevantamentoDados.py

--- LevantamentoDados.py
+++ LevantamentoDados.py
@@ -1,18 +1,26 @@
+from IPython.display import display
 import pandas as pd
+import matplotlib.pyplot as plt
 from pandas.io import sql
 import pyodbc
 from conexao import Retornar_conexao
 
-class Dados:
+class Levdados:
     def __init__(self):
         self.con = Retornar_conexao()
         self.conn = self.con.conexao()
-    
-    def buscar_dados(self): 
-        self.sql = 'SELECT TOP 10 * FROM FactSales'
-        self.dados = sql.read_sql(self.sql, self.conn)
-        self.dados
+        
+    def buscar_vendas(self): 
+        self.vendas = 'SELECT * FROM FactSales'
+        self.dados_vendas = pd.read_sql(self.vendas, self.conn)
+        lista_colunas_vendas = ['StoreKey', 'SalesQuantity']
+        lista_vendas = self.dados_vendas[lista_colunas_vendas]
+        return lista_vendas
+        
+    def buscar_lojas(self):
+        self.lojas = 'SELECT StoreKey, StoreName FROM DimStore'
+        self.dados_lojas = pd.read_sql(self.lojas, self.conn)
+        lista_lojas = self.dados_lojas      
+        return lista_lojas         
 
-                            
-dados = Dados()
-dados.buscar_dados()
+
