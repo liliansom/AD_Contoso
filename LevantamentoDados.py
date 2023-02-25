a335506a76047a2f59b71b33a8218a1f4b0a685a
Lilian Matsumoto <liliansmatsumoto@gmail.com>
2023-02-25 01:51:46 -0300

    Divisão de classes e criação def
    Classe LevDados dividida em classe Busca e classe Análise. Criado método para análise de vendas por subcategoria em class Analise.

--- LevantamentoDados.py
+++ LevantamentoDados.py
@@ -5,18 +5,21 @@ from pandas.io import sql
 import pyodbc
 from conexao import ConexaoBD
 
+# Classe para buscar informações no Banco de Dados
 class Levdados:
     def __init__(self):
         self.con = ConexaoBD()
         self.conn = self.con.conexao()
-        
+    
+    # Método para fazer busca de vendas no banco de dados
     def buscar_vendas(self): 
         self.vendas = 'SELECT * FROM FactSales'
         self.dados_vendas = pd.read_sql(self.vendas, self.conn)
         lista_colunas_vendas = ['StoreKey', 'SalesQuantity', 'ProductKey']
         lista_vendas = self.dados_vendas[lista_colunas_vendas]
         return lista_vendas
-        
+
+    # Método para fazer busca de lojas no banco de dados    
     def buscar_lojas(self):
         self.lojas = 'SELECT * FROM DimStore'
         self.dados_lojas = pd.read_sql(self.lojas, self.conn)
@@ -24,6 +27,7 @@ class Levdados:
         lista_lojas = self.dados_lojas[lista_colunas_lojas]   
         return lista_lojas         
 
+    # Método para fazer busca de produtos no banco de dados
     def buscar_produtos(self):
         self.produtos = 'SELECT * FROM DimProduct'
         self.dados_produtos = pd.read_sql(self.produtos, self.conn)
@@ -38,6 +42,7 @@ class Levdados:
         lista_prodcat = self.mesclar_tabelas(self.lista_produtos, self.lista_subcat, 'ProductSubcategoryKey')
         return lista_prodcat
 
+    #Método para mesclar tabelas
     def mesclar_tabelas(self, tabela1, tabela2, parametro):
         self.tabela1 = tabela1
         self.tabela2 = tabela2
@@ -45,10 +50,15 @@ class Levdados:
         nova_tabela = tabela1.merge(tabela2, on=parametro)
         return nova_tabela
 
+class Analise:
+    def __init__(self):
+        self.dados = Levdados()
+
+    # Método para analisar vendas por loja
     def analisar_vendas(self):
-        lista_vendas = self.buscar_vendas()
-        lista_lojas = self.buscar_lojas()
-        tabela3 = self.mesclar_tabelas(lista_vendas, lista_lojas, 'StoreKey')
+        lista_vendas = self.dados.buscar_vendas()
+        lista_lojas = self.dados.buscar_lojas()
+        tabela3 = self.dados.mesclar_tabelas(lista_vendas, lista_lojas, 'StoreKey')
         self.vendas_lojas = tabela3.groupby('StoreName').sum()
         self.vendas_lojas = self.vendas_lojas[['SalesQuantity']].sort_values('SalesQuantity', ascending=False)
         self.vendas_lojas[:5].plot(figsize=(15,5), kind='bar')
@@ -57,8 +67,26 @@ class Levdados:
         self.conn.close()
         return self.vendas_lojas
 
+    # Método para analisar vendas por subcategoria
+    def analisar_produtos(self):
+        lista_produtos = self.dados.buscar_produtos()
+        lista_vendas = self.dados.buscar_vendas()
+        tabela4 = self.dados.mesclar_tabelas(lista_vendas, lista_produtos,'ProductKey')
+        vendas_por_subcat = tabela4.groupby('ProductSubcategoryName')['SalesQuantity'].sum()
+        # Mesclar produtos com % < 4%
+        pct_vendas_por_subcat = vendas_por_subcat / vendas_por_subcat.sum()
+        subcat_menores_4 = pct_vendas_por_subcat[pct_vendas_por_subcat < 0.04]
+        vendas_outros = vendas_por_subcat[subcat_menores_4.index].sum()
+        vendas_por_subcat['Others'] = vendas_outros
+        vendas_por_subcat.drop(subcat_menores_4.index, inplace=True)
+        vendas_por_subcat = vendas_por_subcat.sort_values(ascending=False)
 
+        vendas_por_subcat.plot(kind='pie', figsize=(8,8), autopct='%1.1f%%', startangle=90)
+        plt.axis('equal')
+        plt.show()
+        self.conn.close()
+        return vendas_por_subcat
 
-busca = Levdados()
-busca.buscar_produtos()
-print(busca.buscar_produtos())
+busca = Analise()
+busca.analisar_produtos()
+
