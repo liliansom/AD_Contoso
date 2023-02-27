6c581409c53ab4db9e266df80dfcfb80428bebb8
Lilian Matsumoto <liliansmatsumoto@gmail.com>
2023-02-26 23:01:05 -0300

    Altarado valor mesclar subcat p/ 3% classe analise

--- ClasseAnalise.py
+++ ClasseAnalise.py
@@ -11,7 +11,7 @@ class Analisar:
         tabela_lojas = self.dados.buscar_lojas()
         tabela3 = self.dados.mesclar_tabelas(tabela_vendas, tabela_lojas, 'StoreKey')
         self.vendas_lojas = tabela3.groupby('StoreName').sum()
-        self.vendas_lojas = self.vendas_lojas.sort_values('SalesQuantity', ascending=False)
+        self.vendas_lojas = self.vendas_lojas[['SalesQuantity']].sort_values('SalesQuantity', ascending=False)
         self.vendas_lojas[:5].plot(figsize=(15,5), kind='bar')
         return self.vendas_lojas
 
@@ -21,9 +21,9 @@ class Analisar:
         tabela_vendas = self.dados.buscar_vendas()
         tabela4 = self.dados.mesclar_tabelas(tabela_vendas, tabela_produtos,'ProductKey')
         vendas_por_subcat = tabela4.groupby('ProductSubcategoryName')['SalesQuantity'].sum()
-        # Mesclar produtos com % < 4%
+        # Mesclar produtos com % < 3%
         pct_vendas_por_subcat = vendas_por_subcat / vendas_por_subcat.sum()
-        subcat_menores_4 = pct_vendas_por_subcat[pct_vendas_por_subcat < 0.04]
+        subcat_menores_4 = pct_vendas_por_subcat[pct_vendas_por_subcat < 0.03]
         vendas_outros = vendas_por_subcat[subcat_menores_4.index].sum()
         vendas_por_subcat['Others'] = vendas_outros
         vendas_por_subcat.drop(subcat_menores_4.index, inplace=True)
